70e24afb6198abfee44acf226585b4ce54bc9950
xiaofen <oulafen@gmail.com>
2014-02-21 15:55:05 +0800

    补全比较结果的测试

--- spec/compare_result_spec.js
+++ spec/compare_result_spec.js
@@ -56,5 +56,25 @@ describe("Compare Result", function () {
         expect(result).toBe('2A0B');
     });
 
+    it("should be 0A0B if random_number=1234 and guess_number=5678", function () {
+
+        var guess_num = 1234;
+        var random_num = 5678;
+        var result = get_compare_result(guess_num, random_num);
+
+        expect(result).toBe('0A0B');
+
+    });
+
+    it("should be 1A3B if random_number=1234 and guess_number=1423", function () {
+
+        var guess_num = 1234;
+        var random_num = 1423;
+        var result = get_compare_result(guess_num, random_num);
+
+        expect(result).toBe('1A3B');
+
+    });
+
 
 });
